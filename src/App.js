7188d8dc9add7462317e31b32a72b45b76101602
Daria <mamontova.dasha@gmail.com>
2019-02-04 14:19:02 +0600

    Добавлены функции отображения задач, удаления задач, добавления новых задач.

--- src/App.js
+++ src/App.js
@@ -1,28 +1,87 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import Task from './Task/Task.js';
+import AddTaskForm from './AddTaskForm/AddTaskForm.js'
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    state = {
+        toDoList: [
+            {id: 1, text: 'О себе 1'},
+            {id: 2, text: 'О себе 2'},
+            {id: 3, text: 'О себе 3'}
+        ],
+
+        currentTask: {text: ''},
+};
+
+
+    removeTask = (id) => {
+        let taskId = this.state.toDoList.findIndex(task => {
+            return task.id === id;
+        });
+
+        const toDoList = [...this.state.toDoList];
+        toDoList.splice(taskId, 1);
+
+        this.setState({
+            ...this.state,
+            toDoList
+        });
+    };
+
+    changeTaskInput = (event) => {
+        let value = event.target.value;
+        let currentTask = {
+            ...this.state.currentTask,
+            text: value
+        };
+        this.setState({
+            ...this.state,
+            currentTask
+        });
+};
+
+    addTask = (event) => {
+        event.preventDefault();
+        let task = {...this.state.currentTask};
+        const now = new Date();
+        task.id = now.getTime();
+        let toDoList = [...this.state.toDoList, task];
+        this.setState({
+            ...this.state,
+            toDoList,
+            currentTask: {
+                text: ''
+            }
+        });
+    };
+
+    isAddButtonDisabled = () => {
+       return this.state.currentTask.text === '';
+};
+
+    render() {
+        return (
+            <div className="App">
+                <div>
+                    <h2>Добавить человека</h2>
+                    <AddTaskForm
+                        task={this.state.currentTask}
+                        onChangeInput={this.changeTaskInput}
+                        onAddTask={this.addTask}
+                        isAddButtonDisabled={this.isAddButtonDisabled()}
+                    />
+                </div>
+                <div>
+                {this.state.toDoList.map((task) => {
+                        return <Task key={task.id} onRemoveTask={() => this.removeTask(task.id)} >
+                            {task.text}
+                        </Task>;
+                    })}
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
