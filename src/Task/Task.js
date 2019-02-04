6f2120bd38953c8173413f32e37b2c77559289b4
Daria <mamontova.dasha@gmail.com>
2019-02-04 14:56:51 +0600

    Добавлены стили.

--- src/Task/Task.js
+++ src/Task/Task.js
@@ -3,12 +3,12 @@ import React, {Component} from 'react';
 class Task extends Component {
     render() {
         return (
-            <div className="card border-secondary align-middle my-2 pt-3 text-secondary">
+            <div className="card w-75 d-inline-block border-secondary align-center my-2 pt-3 text-secondary">
                 <p>{this.props.children}
                     <a href='#' className="ml-4 text-danger" onClick={this.props.onRemoveTask}>
                         <i className="fas fa-trash-alt"></i>
-                    </a><
-                    /p>
+                    </a>
+                    </p>
             </div>
         )
     }
